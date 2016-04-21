05bb8069af880738d47e84d40c41f2ead5b89176
Rodrigo <rodrigo.rodriguez@live.com.ar>
2016-04-20 22:57:35 -0500

    subwe

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ var express  = require("express"),
 
    var allowCrossDomain = function(req, res, next) {
     res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
     res.header('Access-Control-Allow-Headers', 'Content-Type');
     next();
 }
