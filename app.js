011f573f61f37daada55dcb12efb11688522a488
Rodrigo <rodrigo.rodriguez@live.com.ar>
2016-04-20 21:18:30 -0500

    port env2

--- app.js
+++ app.js
@@ -51,9 +51,7 @@ app.get('/panaderias', function(req, res) {
 	}
 });**/
 
-var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080
-var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
- 
-server.listen(server_port, server_ip_address, function () {
-  console.log( "Listening on " + server_ip_address + ", server_port " + port )
+
+server.listen(process.env.port || 3000, function() {
+  console.log("Node server running !! A LIVE");
 });
